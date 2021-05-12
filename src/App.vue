0196cc2b462d2c40b84ced1a2e9294c842fa53f7
Dimitar <37015074+mitaka1210@users.noreply.github.com>
2021-05-12 17:57:16 +0300

    first commit
    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
+
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+//import TestComponent from './components/TestArr.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+
   }
 }
 </script>
---
+++ src/components/TestArr.vue
@@ -0,0 +1,14 @@
+<template>
+	<div>
+		<h1>Hi</h1>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'TestComponent',
+	  props: {
+    msg: String
+  }
+}
+</script>
